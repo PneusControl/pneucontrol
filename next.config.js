040a25770e5994372b23c0be1465d162dcdd4d51
PneusControl <pneuscontrol@gmail.com>
2026-02-01 20:43:05 -0300

    fix: remove swcMinify and align next version to resolve vercel build error

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  swcMinify: false,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true }
 }
