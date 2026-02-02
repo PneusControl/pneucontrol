b2e8d5c6b0f27006c9e1c52707142e077618040d
PneusControl <pneuscontrol@gmail.com>
2026-02-02 16:19:45 -0300

    chore: rename next.config.js to .mjs and update Dockerfile to force clean build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
-
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
   trailingSlash: true,
   typescript: { ignoreBuildErrors: true }
-}
-module.exports = nextConfig
+};
+export default nextConfig;
